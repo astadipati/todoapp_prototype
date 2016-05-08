7bb3dd26ddce0bb666cc8b8ceacb326fda4c3ffc
edo masaru <masaruedo@yahoo.com>
2016-05-08 16:52:49 +0700

    create task modal

--- js/app.js
+++ js/app.js
@@ -3,3 +3,24 @@ $('.show-todolist-modal').click(function(event) {
 
     $('#todolist-modal').modal('show');
 });
+
+$('.show-task-modal').click(function(event) {
+    event.preventDefault();
+
+    $('#task-modal').modal('show');
+});
+
+$(function() {
+    $('input[type=checkbox]').iCheck({
+        checkboxClass: 'icheckbox_square-green',
+        increaseArea: '20%'
+    });
+
+    $('#check-all').on('ifChecked', function(e) {
+        $('.check-item').iCheck('check');
+    });
+
+    $('#check-all').on('ifUnchecked', function(e) {
+        $('.check-item').iCheck('uncheck');
+    });
+});
